9e5a150c09b2054485613ab4fac4ddd2d71389a1
Lee-YuMin <lym@hatiolabs-Mac-mini.local>
2016-09-05 14:21:05 +0900

    프로그레스바 서클 각도 조절 가능

--- src/progress-circle.js
+++ src/progress-circle.js
@@ -3,19 +3,26 @@ export default class ProgressCircle extends scene.Ellipse {
   _draw(context) {
     var {
       value = 0,
+      startAngle = 0,
+      endAngle = 360,
       lineWidth = 20,
       blankStrokeStyle,
       cx, cy, rx, ry
     } = this.model;
 
+    const RADIAN = 0.0174533 / Math.PI
+
     context.beginPath()
-    
+
     this.drawStroke(context)
+    // Angle마다 '-90'와 '-0.5 * Math.PI'가 있는 이유는 0도를 시계의 12시 방향으로 맞추기 위함.
+    var startAngleToRadian = (startAngle - 90) * RADIAN * Math.PI
+    var endAngleToRadian = endAngle * RADIAN * Math.PI
 
     //// / 바깥쪽 원 그리기  ////
     context.strokeStyle = blankStrokeStyle
-    context.ellipse(cx, cy, Math.abs(rx), Math.abs(ry), 0, 0, 2 * Math.PI)
-    
+    context.ellipse(cx, cy, Math.abs(rx), Math.abs(ry), 0, startAngleToRadian, endAngleToRadian - 0.5 * Math.PI)
+
     context.lineWidth = lineWidth
     this.drawFill(context)
     context.stroke()
@@ -25,8 +32,8 @@ export default class ProgressCircle extends scene.Ellipse {
     context.beginPath()
 
     ////  채워지는 원 그리기  ////
-    var percent = Math.min(Math.max(0, (value + (this._anim_alpha || 0)) / 50), 2)   // PI * 2 가 원의 한바퀴 이므로 value가 100일때 2가 되기위해 50으로 나눠줌
-    context.ellipse(cx, cy, Math.abs(rx), Math.abs(ry), 0, - Math.PI / 2, percent * Math.PI - Math.PI / 2)
+    var percent = value / 100
+    context.ellipse(cx, cy, Math.abs(rx), Math.abs(ry), 0, startAngleToRadian, percent * endAngleToRadian - 0.5 * Math.PI)
 
     this.drawStroke(context)
   }
