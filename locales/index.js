f44c4a107433f871e312f801a3d0c93869a05c80
Hearty Oh <heartyoh@hatiolab.com>
2019-01-09 22:10:32 +0900

    fixed #1

---
+++ locales/index.js
@@ -0,0 +1,12 @@
+export default {
+    'en-US': {
+      'label.packman-style': 'packman style'
+    },
+    'ko-KR': {
+      'label.packman-style': '팩맨 모양'
+    },
+    'zh-CN': {
+      'label.packman-style': 'packman style'
+    }
+  }
+  
